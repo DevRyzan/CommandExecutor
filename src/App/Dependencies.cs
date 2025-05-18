8b84eeae905353bd1dc068303cfecd69187afc8d
Rez <rezan.sm@gmail.com>
2025-05-18 15:59:19 +0200

    Featured - Dependency Injection added

---
+++ src/App/Dependencies.cs
@@ -0,0 +1,19 @@
+﻿using App.Manager;
+using Core.Factory;
+using Core.Interfaces;
+using Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Services.Interfaces.IRandomGenerator;
+
+namespace App;
+
+public static class Dependencies
+{
+    public static void Configure(IServiceCollection services)
+    {
+        services.AddSingleton<IRandomGenerator, RandomGenerator>();
+        services.AddSingleton<ICommandLogger, CommandLogger>();
+        services.AddSingleton<ICommandFactory, CommandFactory>();
+        services.AddSingleton<CommandManager>();
+    }
+}
